161a1dd834200007d4f4d16e63cdf839d8267fef
Miltos Skondras <skondram@coventry.ac.uk>
2021-10-01 16:02:25 +0100

    Copy Main from the original repo

---
+++ src/main/java/com/sparta/SortManager/Main.java
@@ -0,0 +1,24 @@
+package com.sparta.SortManager;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] ar1 = Array.arrayGenerator();
+        System.out.println(Arrays.toString(ar1));
+        Sorter theFactory;
+        System.out.println("Would you prefer to sort the array with 1)Bubble Sort or 2)Merge Sort");
+        Scanner scan = new Scanner(System.in);
+        String choice = scan.next();
+        switch(choice){
+            case "1": theFactory = new BubbleSort(); break;
+            case "2": theFactory = new MergeSort(); break;
+            default:  theFactory = null; break;
+        }
+        assert theFactory != null;
+        int[] sortedArray = theFactory.whichSort(ar1);
+        System.out.println(Arrays.toString(sortedArray));
+    }
+
+}
